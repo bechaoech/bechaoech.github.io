8d2c91d206261d8a95bf5f1174df8a1af2b9f3de
bechaoech <203532669+bechaoech@users.noreply.github.com>
2025-05-20 12:22:26 +0700

    20/5

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const favicon = document.getElementById('favicon')
   const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const attentionMessage = 'Be Chao Ech'
 
   document.addEventListener('visibilitychange', function (e) {
     const isPageActive = !document.hidden
